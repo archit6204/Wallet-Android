6229f24dc77111f68e41538a123fd5e0f58a1c78
ARCHIT PANDEY <pandeyrishabh40@gmail.com>
2020-08-11 11:41:11 +0530

    NFC host card emulation Send Money UI prototype.

---
+++ app/src/main/java/com/example/wallet/ui/hostCardEmulator/HostCardEmulatorActivity.kt
@@ -0,0 +1,12 @@
+package com.example.wallet.ui.hostCardEmulator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.wallet.R
+
+class HostCardEmulatorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_host_card_emulator)
+    }
+}
