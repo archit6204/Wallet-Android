0fa7f3eed9fa65a65b563c4a7a4142ccae21fd82
ARCHIT PANDEY <pandeyrishabh40@gmail.com>
2020-08-23 10:43:14 +0530

    UI update - making fields invisible while signIn user.

--- app/src/main/java/com/example/wallet/RegisterActivity.java
+++ app/src/main/java/com/example/wallet/RegisterActivity.java
@@ -5,12 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.wallet.ui.utils.GlobalVariables;
 import com.example.wallet.ui.wallet.UserData;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
@@ -23,17 +26,25 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText etUserName;
     private ProgressBar pbActivityRegister;
     private boolean isUserDataFetched = false;
+    private Button btnSendOtp;
+    private TextInputLayout tilUserName;
+    private TextInputLayout tilUserMobileNo;
+    private TextView tvSignUpMsg;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-
         etUserMobileNo = findViewById(R.id.et_user_mobile_number);
         etUserName = findViewById(R.id.et_user_name);
+        btnSendOtp = findViewById(R.id.btn_send_otp);
+        tilUserMobileNo = findViewById(R.id.til_user_mobile_number);
+        tilUserName = findViewById(R.id.til_user_user_name);
+        tvSignUpMsg = findViewById(R.id.tv_signup_msg);
         pbActivityRegister = findViewById(R.id.pb_activity_register);
         pbActivityRegister.setVisibility(View.VISIBLE);
-        findViewById(R.id.btn_send_otp).setOnClickListener(v -> {
+        btnSendOtp.setOnClickListener(v -> {
             String countryCode = "91";
             String mobileNumber = etUserMobileNo.getText().toString().trim();
             String userName = etUserName.getText().toString().trim();
@@ -56,6 +67,7 @@ public class RegisterActivity extends AppCompatActivity {
             intent.putExtra("userName", userName);
             startActivity(intent);
         });
+
     }
 
     @Override
@@ -65,8 +77,7 @@ public class RegisterActivity extends AppCompatActivity {
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         GlobalVariables globalVariables = (GlobalVariables)getApplication();
         if (currentUser != null && currentUser.getPhoneNumber() != null && currentUser.getPhoneNumber().length() == 13) {
-            Toast.makeText(getApplication(), "Redirecting to home page...",
-                    Toast.LENGTH_SHORT).show();
+            tvSignUpMsg.setText("Getting your stuff ready...please wait!");
             if (currentUser.getDisplayName() != null && !currentUser.getDisplayName().isEmpty()) {
                 globalVariables.setUserName(currentUser.getDisplayName());
                 globalVariables.setMobileNumber(currentUser.getPhoneNumber());
@@ -88,11 +99,13 @@ public class RegisterActivity extends AppCompatActivity {
                             startActivity(intent);
                             pbActivityRegister.setVisibility(View.GONE);
                         } else {
+                            init();
                             pbActivityRegister.setVisibility(View.GONE);
                             Log.d("error", "No such document");
                             isUserDataFetched = false;
                         }
                     } else {
+                        init();
                         Toast.makeText(getApplication(), "Please check your internet connection!",
                                 Toast.LENGTH_SHORT).show();
                         Log.d("failed fetch", "get failed with ", task.getException());
@@ -102,15 +115,28 @@ public class RegisterActivity extends AppCompatActivity {
                 });
 
             } else {
+                init();
                 FirebaseAuth.getInstance().signOut();
                 isUserDataFetched = false;
                 pbActivityRegister.setVisibility(View.GONE);
             }
         } else {
+            init();
             FirebaseAuth.getInstance().signOut();
             isUserDataFetched = false;
             pbActivityRegister.setVisibility(View.GONE);
         }
+    }
+
+    private void init() {
+        tilUserMobileNo.setVisibility(View.VISIBLE);
+        tilUserName.setVisibility(View.VISIBLE);
+        etUserMobileNo.setVisibility(View.VISIBLE);
+        etUserName.setVisibility(View.VISIBLE);
+        btnSendOtp.setVisibility(View.VISIBLE);
+        Toast.makeText(getApplication(), "Please enter details!",
+                Toast.LENGTH_SHORT).show();
+        tvSignUpMsg.setText("Please enter details to proceed.");
     }
 }
 
